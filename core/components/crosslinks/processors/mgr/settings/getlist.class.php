2f5eba58800f53063f023f98c82b7ca458bf753d
Thomas Jakobi <thomas.jakobi@partout.info>
2025-02-11 18:37:30 +0100

    - Check the namespaced settings permission for the listing of the system settings too

--- core/components/crosslinks/processors/mgr/settings/getlist.class.php
+++ core/components/crosslinks/processors/mgr/settings/getlist.class.php
@@ -13,11 +13,23 @@ if (file_exists(MODX_PROCESSORS_PATH . 'system/settings/getlist.class.php')) {
     class_alias(\MODX\Revolution\Processors\System\Settings\GetList::class, \modSystemSettingsGetListProcessor::class);
 }
 
+/**
+ * Class CrosslinksSystemSettingsGetlistProcessor
+ */
 class CrosslinksSystemSettingsGetlistProcessor extends modSystemSettingsGetListProcessor
 {
     public $languageTopics = ['setting', 'namespace', 'crosslinks:setting'];
     public $objectType = 'setting';
     public $primaryKeyField = 'key';
+
+    /**
+     * {@inheritDoc}
+     * @return bool
+     */
+    public function checkPermissions()
+    {
+        return !empty($this->permission) ? $this->modx->hasPermission($this->permission) || $this->modx->hasPermission('crosslinks_' . $this->permission) : true;
+    }
 
     /**
      * {@inheritDoc}

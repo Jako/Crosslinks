50262f547b8fb522eff51f5c856e648da9187e21
Anonymous <noreply@weblate.org>
2025-10-13 21:53:42 +0200

    Translated using Weblate (English)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MODX Extras/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/crosslinks/standard/en/

--- core/components/crosslinks/lexicon/en/default.inc.php
+++ core/components/crosslinks/lexicon/en/default.inc.php
@@ -6,15 +6,11 @@
  * @subpackage lexicon
  */
 $_lang['crosslinks'] = 'Crosslinks';
-
 $_lang['crosslinks.management'] = 'Crosslinks Manager';
-
 $_lang['crosslinks.menu'] = 'Crosslinks';
 $_lang['crosslinks.menu_desc'] = 'Manage Crosslinks';
-
 $_lang['crosslinks.links'] = 'Links';
 $_lang['crosslinks.links_desc'] = 'Manage your crosslinks here. You could edit a grid entry with a click on the icons or with a right click.';
-
 $_lang['crosslinks.link_create'] = 'Add Link';
 $_lang['crosslinks.link_duplicate'] = 'Duplicate Link';
 $_lang['crosslinks.link_parameter'] = 'Parameter';
@@ -23,21 +19,15 @@ $_lang['crosslinks.link_remove_confirm'] = 'Are you sure you want to remove this
 $_lang['crosslinks.link_resource'] = 'Resource';
 $_lang['crosslinks.link_text'] = 'Text';
 $_lang['crosslinks.link_update'] = 'Update Link';
-
 $_lang['crosslinks.jsongrid_key'] = 'Key';
 $_lang['crosslinks.jsongrid_value'] = 'Value';
-
 $_lang['crosslinks.link_err_ae_text'] = 'This text already exists';
 $_lang['crosslinks.link_err_ns_resource'] = 'You need to select a resource';
 $_lang['crosslinks.link_err_ns_text'] = 'You need to specify a text';
 $_lang['crosslinks.link_err_nv_text'] = 'The link contains invalid characters. You could use letters, numbers, a space and the following characters: :.;,-_';
-
 $_lang['crosslinks.settings'] = '<i class="icon icon-cog"></i>';
 $_lang['crosslinks.settings_desc'] = 'Edit the settings of Crosslinks. You can edit the value of a system setting by double-clicking on the ‘Value’ table cell or by right-clicking in the table cell.';
-
 $_lang['crosslinks.debug_mode'] = 'Debug Mode';
-
 $_lang['crosslinks.systemsetting_key_err_nv'] = 'You could only edit settings with the prefix crosslinks.';
 $_lang['crosslinks.systemsetting_usergroup_err_nv'] = 'Only users with a settings permission or a settings_crosslinks permission are allowed to change settings.';
-
 $_lang['crosslinks.duplicate'] = 'Duplicate of';

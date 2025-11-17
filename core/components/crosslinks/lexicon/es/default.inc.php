4d6754fe09e6346f5a2e831f07246417cebfbbc6
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:02 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/crosslinks/lexicon/es/default.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Default Lexicon Entries for Crosslinks
+ *
+ * @package crosslinks
+ * @subpackage lexicon
+ */
+$_lang['crosslinks'] = '';
+$_lang['crosslinks.management'] = '';
+$_lang['crosslinks.menu'] = '';
+$_lang['crosslinks.menu_desc'] = '';
+$_lang['crosslinks.links'] = '';
+$_lang['crosslinks.links_desc'] = '';
+$_lang['crosslinks.link_create'] = '';
+$_lang['crosslinks.link_duplicate'] = '';
+$_lang['crosslinks.link_parameter'] = '';
+$_lang['crosslinks.link_remove'] = '';
+$_lang['crosslinks.link_remove_confirm'] = '';
+$_lang['crosslinks.link_resource'] = '';
+$_lang['crosslinks.link_text'] = '';
+$_lang['crosslinks.link_update'] = '';
+$_lang['crosslinks.jsongrid_key'] = '';
+$_lang['crosslinks.jsongrid_value'] = '';
+$_lang['crosslinks.link_err_ae_text'] = '';
+$_lang['crosslinks.link_err_ns_resource'] = '';
+$_lang['crosslinks.link_err_ns_text'] = '';
+$_lang['crosslinks.link_err_nv_text'] = '';
+$_lang['crosslinks.settings'] = '';
+$_lang['crosslinks.settings_desc'] = '';
+$_lang['crosslinks.debug_mode'] = '';
+$_lang['crosslinks.systemsetting_key_err_nv'] = '';
+$_lang['crosslinks.systemsetting_usergroup_err_nv'] = '';
+$_lang['crosslinks.duplicate'] = '';
